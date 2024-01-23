9ecf7c7dc3f9252a529085887284f1622a971b5c
diansajonuzi23 <148043352+diansajonuzi23@users.noreply.github.com>
2024-01-23 21:28:40 +0100

    Update photofetcher-script.js

--- photofetcher-script.js
+++ photofetcher-script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const baseURL = "https://picsum.photos/300/200";
+  const baseURL = "https://picsum.photos/367/200";
   const photoContainer = document.getElementById("photoContainer");
   const fetchButton = document.getElementById("fetch-button");
   const morephotosButton = document.getElementById("more-photos-button");
   const grayscaleSlider = document.querySelector(".switch input");
 
   function fetchNewPhotos(count) {
-    
+    // shin containerin pa ber fetch fotot
     photoContainer.innerHTML = "";
 
     for (let i = 0; i < count; i++) {
@@ -53,22 +53,22 @@ document.addEventListener("DOMContentLoaded", function () {
   function applyGreyscale() {
     const images = document.querySelectorAll("#photoContainer img");
     images.forEach(img => {
-      img.style.filter = grayscaleSlider.checked ? "grayscale(100%)" : "none";
+      img.style.filter = grayscaleSlider.checked ? "grayscale(100%)" : "none";//si if ? vlereson kushtin nqs esht true fotot behen grayscale : ather del none
     });
   }
 
   fetchButton.addEventListener("click", function () {
-  
+    // i ben fetch fotove pa hequr egzistuseve
     fetchNewPhotos(4);
   });
 
   morephotosButton.addEventListener("click", function () {
-    
+    // shtn foto pa hequr egzistuset
     appendMorePhotos(4);
   });
 
   grayscaleSlider.addEventListener("change", applyGreyscale);
 
- 
+  
   fetchNewPhotos(4);
 });
